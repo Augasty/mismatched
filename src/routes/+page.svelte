a5e8725e52daedb30da9182d2f7a034b8702845a
Augasty <sayaksengupta140@gmail.com>
2023-09-21 02:26:02 +0530

    working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,28 +3,86 @@
   const statetype = ["start", "playing", "paused", "won", "lost"];
 
   let state = statetype[0];
-  let size = 20;
-  let grid = createGrid(size);
+  let size = 20
+  $: grid = state !== "start" ? createGrid(size) : null;
+  if (grid){
 
-  let maxMatches = grid.length / 2;
+    let maxMatches = grid.length / 2;
+  }
 
   let selected = [];
   let matches = [];
 
-  console.log(grid);
+  $:console.log(size,grid);
+
+  // range update
+  let cursize = 20
+  function handleRangeInput(event) {
+    cursize = event.target.value;
+  }
+  
 </script>
 
-{#if state === 'start'}
-<h1>mismatched</h1>
-<button on:click={()=> state = 'playing'}>Play</button>
+
+
+
+
+
+
+
+{#if state === "start"}
+  <h1>mismatched</h1>
+  <div class="range">
+
+    <input type="range" min="10" max="80" step="5" bind:value={size} on:input={handleRangeInput}/>
+    <p>Selected value: {size}</p>
+  </div>
+  <button on:click={() => (state = "playing")}>Play</button>
 {/if}
 
-{#if state === 'playing'}
-<div class="cards">
-    {#each grid as card,idx}
-    <button class="cards">
+{#if state === "playing"}
+
+  <h1>Match pairs</h1>
+  <div class="cards">
+    {#each grid as card, idx}
+      <button class="card">
         <div>{card}</div>
-    </button>
+      </button>
     {/each}
-</div>
+  </div>
 {/if}
+
+{#if state === "lost"}
+  <h1>You lost! 💩</h1>
+  <button on:click={() => (state = "playing")}>Play again</button>
+{/if}
+
+{#if state === "won"}
+  <h1>You win! 💩</h1>
+  <button on:click={() => (state = "playing")}>Play again</button>
+{/if}
+
+
+<style>
+  .range {
+    text-align: center;
+    padding: 20px;
+  }
+
+  .cards{
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    gap: 0.4rem;
+  }
+
+  .card {
+    height: 140px;
+    width: 140px;
+    font-size: 4rem;
+    background-color: var(--bg-2);
+
+    &.selected{
+      border: 4px solid var(--border);
+    }
+  }
+</style>
