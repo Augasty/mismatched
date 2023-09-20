4314acc9fc65b5f70e195ded1c528835084fe18c
Augasty <sayaksengupta140@gmail.com>
2023-09-21 01:17:13 +0530

    emojis visible, functionality not added yet. changes will be made on another branch

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,30 @@
 <script>
-    import {createGrid} from './emoji'
-    const statetype = ['start','playing','paused','won','lost']
+  import { createGrid } from "./emoji";
+  const statetype = ["start", "playing", "paused", "won", "lost"];
 
-    let state = statetype[0]
-    let size = 20
-    let grid = createGrid(size)
+  let state = statetype[0];
+  let size = 20;
+  let grid = createGrid(size);
 
+  let maxMatches = grid.length / 2;
 
-    console.log(grid)
+  let selected = [];
+  let matches = [];
+
+  console.log(grid);
 </script>
+
+{#if state === 'start'}
+<h1>mismatched</h1>
+<button on:click={()=> state = 'playing'}>Play</button>
+{/if}
+
+{#if state === 'playing'}
+<div class="cards">
+    {#each grid as card,idx}
+    <button class="cards">
+        <div>{card}</div>
+    </button>
+    {/each}
+</div>
+{/if}
